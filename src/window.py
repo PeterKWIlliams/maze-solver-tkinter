1038904d08f9d0e3b24ba7d5de5fbd56a8c7ded3
Peter Williams <peterkennethwilliams@gmail.com>
2024-08-01 19:57:53 +0100

    Cell Update
    - cell class updated to draw white on walls that are not to be shown
    - cell class ability to redraw itself added

--- src/window.py
+++ src/window.py
@@ -1,6 +1,6 @@
 from tkinter import BOTH, Canvas, Tk
 
-from draw import Line, Point
+from .draw import Line, Point
 
 
 class Window:
@@ -42,22 +42,32 @@ class Cell:
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
-
+        if self._win is None:
+            return
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
         self._y2 = y2
-
         p1, p2, p3, p4 = Point(x1, y1), Point(x2, y2), Point(x1, y2), Point(x2, y1)
-
         if self.has_left_wall:
             self._win.draw_line(Line(p1, p3), "red")
+        else:
+            self._win.draw_line(Line(p1, p3), "white")
         if self.has_right_wall:
             self._win.draw_line(Line(p2, p4), "red")
+        else:
+            self._win.draw_line(Line(p2, p4), "white")
         if self.has_bottom_wall:
-            self._win.draw_line(Line(p1, p4), "red")
-        if self.has_top_wall:
             self._win.draw_line(Line(p3, p2), "red")
+        else:
+            self._win.draw_line(Line(p3, p2), "white")
+        if self.has_top_wall:
+            self._win.draw_line(Line(p1, p4), "red")
+        else:
+            self._win.draw_line(Line(p1, p4), "white")
+
+    def redraw(self):
+        self.draw(self._x1, self._y1, self._x2, self._y2)
 
     def get_centre(self):
         if self._x1 is None or self._y1 is None or self._x2 is None or self._y2 is None:
@@ -65,7 +75,7 @@ class Cell:
         return ((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2)
 
     def draw_move(self, to_cell, undo=False):
-        color = "blue" if not undo else "red"
+        color = "red" if undo else "gray"
         centre_1, centre_2 = to_cell.get_centre(), self.get_centre()
 
         if centre_1 and centre_2:
